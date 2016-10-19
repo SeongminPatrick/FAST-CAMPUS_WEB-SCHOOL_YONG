44f12af974c80f217f393fe8a4633bdb8f2031f6
yongpilmoon <m71661@gmail.com>
2016-10-19 23:26:16 +0900

    Add bookmark,blog None exception

--- DailyStudy/DJANGO/django_girls/django_app/video/apis/youtube.py
+++ DailyStudy/DJANGO/django_girls/django_app/video/apis/youtube.py
@@ -3,21 +3,13 @@ from googleapiclient.errors import HttpError
 from oauth2client.tools import argparser
 from video.models import Video
 
-
 DEVELOPER_KEY = "AIzaSyDiarbwPOxSkXmNPfdv8UtHcZM6KySpk34"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 
 def youtube_search(keyword, page_token, max_results=10):
-    """
-    youtube_search함수 개선
 
-    1. youtube_search 함수의 arguments에 pageToken 추가
-    2. 받은 pageToken값을 youtube.search()실행 시 list의 인자로 추가
-    3. search뷰에서 request.GET에 pageToken값을 받아오도록 설정
-    4. template에서 이전페이지/다음페이지 a태그 href에 GET parameter가 추가되도록 설정
-    """
     youtube = build(
         YOUTUBE_API_SERVICE_NAME,
         YOUTUBE_API_VERSION,
