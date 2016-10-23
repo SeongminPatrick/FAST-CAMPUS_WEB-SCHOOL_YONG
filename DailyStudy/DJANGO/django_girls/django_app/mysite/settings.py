91f171fe33d3da643b55e7445935e42508de54d8
yongpilmoon <m71661@gmail.com>
2016-10-23 23:17:38 +0900

    Add facebook friend rank and my post list function

--- DailyStudy/DJANGO/django_girls/django_app/mysite/settings.py
+++ DailyStudy/DJANGO/django_girls/django_app/mysite/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'blog',
     'member',
     'video',
+    'sns',
 ]
 
 MIDDLEWARE = [
--- DailyStudy/DJANGO/django_girls/django_app/sns/views/facebook.py
+++ DailyStudy/DJANGO/django_girls/django_app/sns/views/facebook.py
@@ -1,16 +1,19 @@
 import requests
-import json
+from django.db.models import Q
 from django.http import HttpResponse
-from django.urls import reverse
-from member.apis import facebook
 from django.shortcuts import render
+from django.urls import reverse
+from sns.models import FriendsRank
+from member.apis import facebook
 
 __all__ = [
-    'friends_ranking'
+    'update_friends_ranking',
+    'show_friends_ranking',
+    'show_mypost'
 ]
 
 
-def friends_ranking(request):
+def update_friends_ranking(request):
     if request.GET.get('error'):
         return HttpResponse('사용자 로그인 거부')
     if request.GET.get('code'):
@@ -21,8 +24,8 @@ def friends_ranking(request):
         code = request.GET.get('code')
         access_token = facebook.get_access_token(code, redirect_uri)
         user_id = facebook.get_user_id_from_token(access_token)
-        url_request_feed = 'https://graph.facebook.com/v2.8/{user_id}/feed?' \
-                           'fields=comments&' \
+        url_request_feed = 'https://graph.facebook.com/v2.8/{user_id}/feed?fields=' \
+                           'comments{{from{{name,picture.type(normal)}}}}&' \
                            'access_token={access_token}'.format(
                             user_id=user_id,
                             access_token=access_token,
@@ -30,34 +33,75 @@ def friends_ranking(request):
 
         r = requests.get(url_request_feed)
         dict_feed_info = r.json()
-        json_data = json.dumps(dict_feed_info, indent=2)
-
         temp_custom_data = []
-        for feed in dict_feed_info['data']:
-            if feed.get('comments'):
-                for data in feed.get('comments')['data']:
-                    data['from']['message'] = data['message']
-                    temp_custom_data.append(data['from'])
+
+        while dict_feed_info.get('paging'):
+            for feed in dict_feed_info['data']:
+                if feed.get('comments'):
+                    for data in feed.get('comments')['data']:
+                        data['from']['picture_url'] = data['from']['picture']['data']['url']
+                        temp_custom_data.append(data['from'])
+            url_request_feed = dict_feed_info['paging']['next']
+            r = requests.get(url_request_feed)
+            dict_feed_info = r.json()
 
         custom_data = []
         for i in temp_custom_data:
             cnt = 0
-            temp_message = []
             if not (i['id'] in [i['id'] for i in custom_data]) and (i['id'] != user_id):
                 for j in temp_custom_data:
                     if i['id'] == j['id']:
-                        temp_message.append(j['message'])
                         cnt += 1
-                custom_data.append({
-                    'id': i['id'],
-                    'name': i['name'],
-                    'cnt': cnt,
-                    'message': temp_message
-                })
+                obj, created = FriendsRank.objects.update_or_create(
+                    friends_id=i['id'],
+                    defaults={
+                        'friends_id': i['id'],
+                        'name': i['name'],
+                        'picture_url': i['picture_url'],
+                        'comment_count': cnt,
+                    })
 
-        custom_data = sorted(custom_data, key=lambda k: k['cnt'], reverse=True)
+        return render(request, 'sns/update_friends_ranking.html', {})
 
-        context = {
-            'custom_data': custom_data
+
+def show_friends_ranking(request):
+    friends = FriendsRank.objects.all().order_by('-comment_count')
+    context = {
+        'friends': friends
+    }
+
+    return render(request, 'sns/show_friends_ranking.html', context)
+
+
+def show_mypost(request):
+    if request.GET.get('error'):
+        return HttpResponse('사용자 거부')
+    if request.GET.get('code'):
+        redirect_uri = 'http://{host}{url}'.format(
+            host=request.META['HTTP_HOST'],
+            url=reverse('sns:show_mypost')
+        )
+        code = request.GET.get('code')
+        access_token = facebook.get_access_token(code, redirect_uri)
+        user_id = facebook.get_user_id_from_token(access_token)
+        url_request_post = 'https://graph.facebook.com/v2.8/{user_id}/posts?' \
+                           'access_token={access_token}'.format(
+                            user_id=user_id,
+                            access_token=access_token,
+                            )
+        r = requests.get(url_request_post)
+        dict_post_info = r.json()
+        print(dict_post_info)
+
+        post_data_list = []
+        for post in dict_post_info['data']:
+            if post.get('message'):
+                post_data_list.append({
+                     'message':post['message'],
+                     'created_time':post['created_time'],
+                    })
+
+        context ={
+            'posts': post_data_list,
         }
-        return render(request, 'sns/friends_ranking.html', context)
+        return render(request, 'sns/show_my_posts.html', context)
